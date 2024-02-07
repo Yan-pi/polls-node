f95c8eeb7a4ee897eb97a3a1e2ae4c4b913a6076
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2024-02-07 20:39:05 -0300

    feat: create vote on poll feature, schema and migration

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 import { prisma } from "../../lib/prisma";
+import { randomUUID } from "node:crypto";
 import { FastifyInstance } from "fastify";
 
 export async function voteOnPoll(app: FastifyInstance) {
@@ -15,6 +16,53 @@ export async function voteOnPoll(app: FastifyInstance) {
     const { pollId } = voteOnPollParams.parse(request.params);
     const { pollOptionId } = voteOnPollBody.parse(request.body);
 
-    return reply.status(201).send();
+    let sessionId = request.cookies.sessionId;
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      });
+
+      if (
+        userPreviousVoteOnPoll &&
+        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+      ) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          },
+        });
+      } else if (userPreviousVoteOnPoll) {
+        return reply
+          .status(400)
+          .send({ message: "you already voted on this option for this poll" });
+      }
+    }
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30, //30 Days
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
+
+    return reply.status(201).send({ message: `you voted on ${pollOptionId}` });
   });
 }
