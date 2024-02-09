726a2629f3cefce7e24a57a18c631f3fb6db5a24
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2024-02-08 22:47:44 -0300

    feat: use redis to see how many votes per option

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,9 +2,11 @@ import z from "zod";
 import { prisma } from "../../lib/prisma";
 import { randomUUID } from "node:crypto";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
+
     const voteOnPollBody = z.object({
       pollOptionId: z.string().uuid(),
     });
@@ -28,19 +30,17 @@ export async function voteOnPoll(app: FastifyInstance) {
         },
       });
 
-      if (
-        userPreviousVoteOnPoll &&
-        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
-      ) {
+      if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
         await prisma.vote.delete({
           where: {
             id: userPreviousVoteOnPoll.id,
           },
         });
+        
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
-        return reply
-          .status(400)
-          .send({ message: "you already voted on this option for this poll" });
+        
+        return reply.status(400).send({ message: "you already voted on this option for this poll" });
       }
     }
 
@@ -55,6 +55,12 @@ export async function voteOnPoll(app: FastifyInstance) {
       });
     }
 
+    const pollOption = await prisma.pollOption.findUnique({
+      where: {
+        id: pollOptionId,
+      },
+    });
+
     await prisma.vote.create({
       data: {
         sessionId,
@@ -63,6 +69,8 @@ export async function voteOnPoll(app: FastifyInstance) {
       },
     });
 
-    return reply.status(201).send({ message: `you voted on ${pollOptionId}` });
+    await redis.zincrby(pollId, 1, pollOptionId);
+
+    return reply.status(201).send({ message: `you voted on ${pollOption?.title}` });
   });
 }
