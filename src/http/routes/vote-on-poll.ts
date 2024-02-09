51cdb9aa3439761bdb4a00b69b906e3a54a07d14
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2024-02-09 01:52:12 -0300

    refactor: implement pub-sub and websocket feature on vote-on-poll

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,10 +3,10 @@ import { prisma } from "../../lib/prisma";
 import { randomUUID } from "node:crypto";
 import { FastifyInstance } from "fastify";
 import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
-
     const voteOnPollBody = z.object({
       pollOptionId: z.string().uuid(),
     });
@@ -30,16 +30,23 @@ export async function voteOnPoll(app: FastifyInstance) {
         },
       });
 
-      if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+      if ( userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+        
         await prisma.vote.delete({
           where: {
             id: userPreviousVoteOnPoll.id,
           },
         });
-        
-        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+        const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes),
+        });
+
       } else if (userPreviousVoteOnPoll) {
-        
+
         return reply.status(400).send({ message: "you already voted on this option for this poll" });
       }
     }
@@ -69,7 +76,12 @@ export async function voteOnPoll(app: FastifyInstance) {
       },
     });
 
-    await redis.zincrby(pollId, 1, pollOptionId);
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
+    });
 
     return reply.status(201).send({ message: `you voted on ${pollOption?.title}` });
   });
