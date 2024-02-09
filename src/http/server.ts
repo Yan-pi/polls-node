349df82487d3a10bd62cec6aeca6a919a5e268f6
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2024-02-09 01:52:25 -0300

    refactor: adjust routes for app

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,17 +3,20 @@ import cookie from "@fastify/cookie";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
-
+import websocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-results";
 const app = fastify();
 
 app.register(cookie, {
   secret: "an-unique-string",
   hook: "onRequest",
 });
+app.register(websocket)
 
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server listening on port 3333!");
